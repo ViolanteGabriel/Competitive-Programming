ee3081f99707390d6bc9cbf308b945087a24ac2c
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2024-03-27 23:33:10 -0300

    message

--- 800-Problems/Shapes.cpp
+++ 800-Problems/Shapes.cpp
@@ -14,19 +14,18 @@ int main(int argc, char const *argv[])
         {
             cin >> line;
             gridLines.push_back(line);
-            
         }
-        occurence_1 = 0, occurrence_2 = 0;
         for (int j = 0; j < gridSize; j++)
         {
             occurence_1 = count(gridLines[j].begin(), gridLines[j].end(), '1');
             if (occurence_1 > 0 && j < gridSize)
             {
                 occurrence_2 = count(gridLines[j + 1].begin(), gridLines[j + 1].end(), '1');
-                if (occurence_1 == occurrence_2) cout << "SQUARE" << endl;
-                else if (occurence_1 != occurrence_2) cout << "TRIANGLE" << endl;
+                if (occurence_1 == occurrence_2) {cout << "SQUARE" << endl; break;}
+                else {cout << "TRIANGLE" << endl; break;}
             }
         }
+        gridLines.clear();
     }
     return 0;
 }
