b30b1031993deed3ed470e386e481e9e83e2974c
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2024-03-31 21:11:05 -0300

    First 1200 Problem

--- 1200-Problems/SumOfSum.cpp
+++ 1200-Problems/SumOfSum.cpp
@@ -1,26 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAX = 200'007;
+int sumOfAllNumbers[MAX];
 
 int main(int argc, char const *argv[]){
-    int nTests, maxNumber, aux, sum = 0, digitsSum = 0;
+    int nTests, number, aux, digitsSum = 0;
     cin >> nTests;
+    for (int j = 1; j < MAX; j++)
+    {
+        aux = j;
+        do
+        {
+            digitsSum = (aux % 10) + digitsSum;
+            aux = aux / 10;
+        } while (aux / 10 != 0 || aux % 10 != 0);
+        if (digitsSum == 0) digitsSum = 1;
+        if (j >= 2) sumOfAllNumbers[j - 1] = digitsSum + sumOfAllNumbers[j - 2];
+        else sumOfAllNumbers[j - 1] = digitsSum;
+        digitsSum = 0;
+    }
+
     for (int i = 0; i < nTests; i++)
     {
-        cin >> maxNumber;
-        for (int j = 1; j <= maxNumber; j++)
-        {
-            aux = j;
-            do
-            {
-                digitsSum = (aux % 10) + digitsSum;
-                aux = aux / 10;
-            } while (aux / 10 != 0 || aux % 10 != 0);
-            if (digitsSum == 0) digitsSum = 1;
-            sum = sum + digitsSum;
-            digitsSum = 0;
-        }
-        cout << sum << endl;
-        sum = 0;
+        cin >> number;
+        cout << sumOfAllNumbers[number - 1] << endl;
     }
     return 0;
 }
