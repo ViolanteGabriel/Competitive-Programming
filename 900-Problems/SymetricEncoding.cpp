19e60c18a9411574cc92df50a7587ffee5901c20
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2024-05-20 17:10:58 -0300

    more

--- 900-Problems/SymetricEncoding.cpp
+++ 900-Problems/SymetricEncoding.cpp
@@ -9,7 +9,23 @@
 using namespace std;
 
 void solve(){
-    
+    int n; cin >> n;
+    string s; cin >> s;
+
+    set<char> aux(s.begin(), s.end());
+
+    string saux(aux.begin(), aux.end());
+
+    map<char, char> m;
+    for (int i = 0, j = saux.size() - 1; j >= i; i++, j--){
+        m[saux[i]] = saux[j];
+        m[saux[j]] = saux[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << m[s[i]];
+    cout << endl;
+}
 
 int32_t main(){
     int t;
