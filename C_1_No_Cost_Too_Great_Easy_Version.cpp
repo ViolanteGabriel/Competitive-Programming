3507bd42031609737b3481351b10681e66fbe4c0
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2025-10-21 22:25:14 -0300

    C div 3

---
+++ C_1_No_Cost_Too_Great_Easy_Version.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define for_each(i, container) for(auto i : container)
+#define int long long
+#define endl '\n'
+#define ll long long
+
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> n[i];
+    
+
+}
+
+int32_t main(){
+    int t;
+    cin >> t;
+    while(--t)
+        solve();
+    solve();
+    return 0;
+}
