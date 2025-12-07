588a66f756d34106cd6a341614cf6734a8b58aef
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2025-12-07 13:18:27 -0300

    auxilliar functions template

--- Template/divisibilidade.cpp
+++ Template/divisibilidade.cpp
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 
-#define forn(i, n) for(int i = 0; i < n; i++)
-#define for_each(i, container) for(auto i : container)
-#define int long long
+// Definitions and Macros
+#define int long long 
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define forn(i, n) for (int i = 0; i < n; i++)
 #define endl '\n'
-#define ll long long
 
 using namespace std;
 
-// Verifica se um numero eh primo:
-bool ehPrimo(int numero) {
-    if (numero <= 1) return false;
+// Verify if a number is Prime, O(sqrt(n)).
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
 
-    for (int i = 1; i*i <= numero; i++)
-    {
-        if (numero % i == 0 and (i != 1 and i != numero)) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
     }
     return true;
 }
 
-// Retorna um set com os divisores de um numero;
-unordered_set<int> divisores(int number) {
-    unordered_set<int> divisores;
-    if (number == 0) {
-        divisores.insert(0);
-        return divisores;
-    } 
-
-    for (int i = 1; i*i <= number; i++)
-    {
-        if (number % i == 0) {
-            divisores.insert(i);
-            divisores.insert(number/i);
+// Returns the divisors of a given number, O(sqrt(N)).
+vector<int> getDivisors(int n) {
+    vector<int> divs;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            divs.pb(i);
+            if (i * i != n) {
+                divs.pb(n / i);
+            }
         }
     }
-    return divisores;
+    return divs;
 }
