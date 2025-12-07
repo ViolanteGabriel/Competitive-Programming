7fb7295b002afaf5e9d39d29cd075a7ab9150c5c
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2025-12-07 13:15:08 -0300

    New CF Template

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -1,26 +1,58 @@
-#include <bits/stdc++.h>
+/*
+    Author: Gabriel Violante
+    CF Handle: Kordirior
+    Federal University of Minas Gerais
+*/
 
-#define forn(i, n) for(int i = 0; i < (n); i++)
-#define for_each(i, container) for(auto i : container)
-#define int long long
-#define endl '\n'
-#define ll long long
+#include <bits/stdc++.h>
 
 using namespace std;
 
+// Definitions and Macros
+#define int long long 
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define endl '\n'
 
-void solve(){
-    
+// Useful Constants
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+
+// Fast I/O
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 }
 
-int32_t main(){
-    int t;
+// Debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+// Problem Solution
+void solve() {
+    int n;
+    cin >> n;
+    
+
+}
+
+// Main
+int32_t main() {
+    fast_io();
+    
+    int t = 1;
     cin >> t;
-    while(--t)
+    
+    while (t--) {
         solve();
-    solve();
+    }
+    
     return 0;
 }
-
-
-
