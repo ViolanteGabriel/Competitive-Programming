0695522e80540a91117cfe79eae0aea7278679f2
ViolanteGabriel <gabrielmachado2005@hotmail.com>
2025-12-16 15:23:19 -0300

    Green Problem | Day 7

--- D_Retaliation.cpp
+++ D_Retaliation.cpp
@@ -44,7 +44,58 @@ void solve() {
     forn(i, n)
         cin >> a[i];
 
-    
+    /*
+    note que todos os numeros necessariamente devem se tornar 0`s
+    simultanemanete, nesse sentido, temos um numero fixo de operacoes
+    x e y sendo:
+    x = numero de operacoes do Tipo I   (ai - i)
+    y = numero de operacoes do Tipo II  (ai - (n-i+1))
+
+    Nesse sentido, note que podemos calcular cada termo com a seguinte
+    equacao:
+    a1 = x + Ny
+    a2 = 2x + (N - 1)y
+    a3 = 3x + (N - 2)y
+    .
+    .
+    .
+    an = Nx + y
+
+    observe que se existirem dois valores quaisqueres de x e y que
+    satisfacam todos os casos, a resposta eh sim. Note que devemos
+    verificar se x e y são ambos positivos também.
+
+    tome a1 = a, a2 = b.
+
+    a = x + ny
+    x = a - ny
+
+    b = 2x + (n-1)y
+    b = 2(a-ny) + (n-1)y
+    b = 2a - 2ny + ny - y
+    b = 2a - ny - y
+    b = 2a - y(n + 1)
+    (b - 2a)/(n+1) = -y
+
+    y = -(b-2a)/(n+1)
+    x = a - ny 
+
+    depois disso, basta veriricar se os valores de x e y sao positivos e se satisfazem
+    a formula para todos os elementos do vetor.
+    */
+
+    int y = -(a[1] - 2*a[0])/(n + 1);
+    int x = a[0] - n*y;
+
+    if (x < 0 or y < 0) {cout << "NO" << endl; return;}
+    forn(i, n) {
+        if (a[i] != x*(i + 1) + y*(n - (i+1) + 1)){
+            cout << "NO" << endl;
+            return;
+        } 
+    }
+    cout << "YES" << endl;
+    return;
 }
 
 // Main
